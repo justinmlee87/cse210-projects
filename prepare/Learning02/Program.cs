3e0c4944a70e5474a7c14d1288d97fd9efd5caa1
justinmlee87 <justinmlee87@gmail.com>
2023-05-03 12:51:12 -0400

    learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+     job job1 = new job();
+     job1._company = "Disney";
+     job1._jobTitle = "Software Engineer";
+     job1._startYear = 2017;
+     job1._endYear = 2023;
+
+
+     job job2 = new job();
+     job2._company = "Apple";
+     job2._jobTitle = "Cybersecurity";
+     job2._startYear = 2015;
+     job2._endYear = 2017;
+
+
+     Resume myResume = new Resume();
+     myResume._name = "Justin Lee";
+
+     myResume._jobs.Add(job1);
+
+     myResume.Display();
     }
 }
