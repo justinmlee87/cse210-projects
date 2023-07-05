2c84c34d356fdb75881caac3004854681ec3786f
justinmlee87 <justinmlee87@gmail.com>
2023-07-05 10:48:06 -0400

    First Two Projects

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
-class Program
+namespace VideoComments
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        static void Main(string[] args)
+        {
+            List<Video> videos = new List<Video>();
+
+            Video v1 = new Video("How to Play Guitar", "Justin Lee", 600);
+            v1.Comments.Add(new Comment("Tanner", "This is extremely helpful!"));
+            v1.Comments.Add(new Comment("Tyler", "Can you do a follow up video focusing more on chords?"));
+            v1.Comments.Add(new Comment("Steve", "It's going to take a lot of practice!"));
+            v1.Comments.Add(new Comment("Cheyanne", "I hope I can play like that some day!"));
+            videos.Add(v1);
+
+            Video v2 = new Video("Intro to Chords", "Justin Lee", 700);
+            v2.Comments.Add(new Comment("Wendy", "I really like to use the D chord!"));
+            v2.Comments.Add(new Comment("David", "Great examples of different chords!"));
+            v2.Comments.Add(new Comment("Jeannette", "Thanks for the examples!"));
+            videos.Add(v2);
+
+            Video v3 = new Video("Picking Practice", "Justin Lee", 500);
+            v3.Comments.Add(new Comment("Sean", "These are great warm up tips."));
+            v3.Comments.Add(new Comment("Brian", "Hopefully I will get better!"));
+            videos.Add(v3);
+
+            foreach (Video v in videos)
+            {
+                Console.WriteLine("Title: " + v.Title);
+                Console.WriteLine("Author: " + v.Author);
+                Console.WriteLine("Length: " + v.Length + " seconds");
+                Console.WriteLine("Number of comments: " + v.GetNumComments());
+                Console.WriteLine("Comments:");
+
+                foreach (Comment c in v.Comments)
+                {
+                    Console.WriteLine("- " + c.Name + ": " + c.Text);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,36 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Create some products
+        Product product1 = new Product("Guitar", "P1", 10.0, 2);
+        Product product2 = new Product("Amplifier", "P2", 5.0, 3);
+
+        // Create some customers
+        Address address1 = new Address("2228 Gator Drive", "Orlando", "FL", "USA");
+        Customer customer1 = new Customer("Justin Lee", address1);
+
+        Address address2 = new Address("123 Rocker Street", "Amsterdam", "NL", "Netherlands");
+        Customer customer2 = new Customer("Sean Smith", address2);
+
+        // Create some orders
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+
+        // Print packing labels, shipping labels, and total prices for the orders
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine("Total price: $" + order1.GetTotalPrice());
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine("Total price: $" + order2.GetTotalPrice());
     }
 }
